62ca76bea46b73b66f839c32111647f0b9c53b43
superzrr <superzrr@gmail.com>
2017-03-03 20:22:30 +0800

    add bootstrap-alert

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "Morning, Hello"
+    flash[:warning] = "Evening,Hello"
+    flash[:alert] = "Noon, Hello"
+  end
 end
